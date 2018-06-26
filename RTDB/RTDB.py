56e33127a9421b3bf4e30daa75dd93a95d209fa5
MAMA <marc.matten@rwth-aachen.de>
2018-06-26 22:46:21 +0200

    added real time database

---
+++ RTDB/RTDB.py
@@ -0,0 +1,10 @@
+import pygame
+
+class RTDB:
+    def __init__(self):
+        print('RTDB initialised!')
+
+    def initialise(self, data):
+        temp = list(data.items())
+        for i in range(0,len(data)):
+            self.__setattr__(temp[i][0], temp[i][1])
