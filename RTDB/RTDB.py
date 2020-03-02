8eb20450b5d5e499c5c3839cd3e79539ca65cd9d
Marc <marc@matten-online.de>
2020-03-02 22:47:49 +0100

    iRacing RTDB varibales are now querried from a list which is an RTDB variable itself

--- RTDB/RTDB.py
+++ RTDB/RTDB.py
@@ -9,16 +9,19 @@ class RTDB:
     def __init__(self):
         timeStr = time.strftime("%H:%M:%S", time.localtime())
         self.initData = list()
+        self.queryData = list()
         self.StopDDU = True
         self.StartDDU = False
         self.timeStr = time.strftime("%H:%M:%S", time.localtime())
         print(timeStr + ': RTDB initialised!')
 
-    def initialise(self, data):
+    def initialise(self, data, BQueryData):
         temp = list(data.items())
         for i in range(0, len(data)):
             self.__setattr__(temp[i][0], temp[i][1])
         self.initData.extend(temp)
+        if BQueryData:
+            self.queryData.extend(list(data.keys()))
 
     def get(self, string):
         return self.__getattribute__(string)
@@ -72,18 +75,17 @@ class RTDB:
         self.track.loadJson(trackPath)
         self.map = self.track.map
 
-        self.initialise(data)
+        self.initialise(data, False)
 
         print(time.strftime("%H:%M:%S", time.localtime()) + ': Loaded RTDB snapshot: ' + name +'.json')
 
 
 # create thread to update RTDB
 class iRThread(threading.Thread):
-    def __init__(self, rtdbObj, keys, rate):
+    def __init__(self, rtdbObj, rate):
         threading.Thread.__init__(self)
         self.rate = rate
         self.db = rtdbObj
-        self.keys = keys
         self.ir = irsdk.IRSDK()
 
     def run(self):
@@ -91,8 +93,8 @@ class iRThread(threading.Thread):
             self.db.startUp = self.ir.startup()
             if self.db.startUp:
                 self.ir.freeze_var_buffer_latest()
-                for i in range(0, len(self.keys)):
-                    self.db.__setattr__(self.keys[i], self.ir[self.keys[i]])
+                for i in range(0, len(self.db.queryData)):
+                    self.db.__setattr__(self.db.queryData[i], self.ir[self.db.queryData[i]])
                 self.ir.unfreeze_var_buffer_latest()
             else:
                 self.ir.shutdown()
